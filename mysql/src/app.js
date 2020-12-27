8f1bf6747ab956c3a72eb1bd301260c4e7d1ef6b
yhs <mvmaniaz@gmail.com>
2020-12-27 18:14:44 +0900

    [추가] MySQL 진행 중

---
+++ mysql/src/app.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const nunjucks = require('nunjucks');
+
+const {sequelize} = require('./models');
+
+const app = express();
+app.set('port', process.env.PORT || 3001);
+app.set('view engine', 'html');
+
+nunjucks.configure('src/views', {
+  express: app,
+  watch: true
+});
+
+sequelize
+  .sync({force: false})
+  .then(() => {
+    console.log('데이터베이스 연결 성공');
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+// 여기까지 왔다는 것은 매칭되는 라우터 경로가 없는 것으로 판단함
+// 그래서 404로 에러 미들웨어로 던짐
+app.use((req, res, next) => {
+  const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+  error.status = 404;
+  next(error);
+});
+
+// 에러 처리 미들웨어
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+app.listen(app.get('port'), () => {
+  console.log(app.get('port'), '번 포트에서 대기 중');
+});
---
+++ mysql/src/models/user.js
@@ -0,0 +1,46 @@
+const Sequelize = require('sequelize');
+
+module.exports = class User extends Sequelize.Model {
+  static init(sequelize) {
+    return super.init(
+      {
+        name: {
+          type: Sequelize.STRING(20),
+          allowNull: false,
+          unique: true
+        },
+        age: {
+          type: Sequelize.INTEGER.UNSIGNED,
+          allowNull: false
+        },
+        married: {
+          type: Sequelize.BOOLEAN,
+          allowNull: false
+        },
+        comment: {
+          type: Sequelize.TEXT,
+          allowNull: true
+        },
+        created_at: {
+          type: Sequelize.DATE, // DATETIME
+          allowNull: false,
+          defaultValue: Sequelize.NOW
+        }
+      },
+      {
+        sequelize,
+        timestamps: false, // true로 하면 createdAt, updatedAt이 자동으로 생겨지고 자동으로 값도 넣어줌
+        underscored: false, // 언더스코어 문법으로 컬럼 생성 여부
+        paranoid: false, // true로 하면 deleteAt이 자동으로 생겨지고 삭제 할 경우 true로 변경해줌
+        modelName: 'User',
+        tableName: 'users', // 모델 이름을 기준으로 복수형으로 자동으로 생성됨?
+        charset: 'utf8',
+        collate: 'utf8_general_ci'
+      }
+    );
+  }
+
+  static associate(db) {
+    db.User.hasMany(db.Comment, {foreignKey: 'commenter', sourceKey: 'id'});
+  }
+};
