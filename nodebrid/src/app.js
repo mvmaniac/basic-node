8d674ecba94c0d66b513bc905d70657c00d1b941
yhs <mvmaniaz@gmail.com>
2021-01-06 00:24:59 +0900

    익스프레스로 SNS 서비스 만들기 진행 중

---
+++ nodebrid/src/app.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
+const path = require('path');
+const session = require('express-session');
+const nunjucks = require('nunjucks');
+const dotenv = require('dotenv');
+const passport = require('passport');
+
+dotenv.config();
+
+const pageRouter = require('./routes/page');
+const authRouter = require('./routes/auth');
+const {sequelize} = require('./models');
+const passportConfig = require('./passport');
+
+passportConfig(); // 패스포트 설정
+
+const app = express();
+
+app.set('port', process.env.PORT || 8001);
+app.set('view engine', 'html');
+nunjucks.configure('src/views', {
+  express: app,
+  watch: true
+});
+
+sequelize
+  .sync({force: false})
+  .then(() => {
+    console.log('데이터베이스 연결 성공');
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+      httpOnly: true,
+      secure: false
+    }
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('/', pageRouter);
+app.use('/auth', authRouter);
+
+// 404 처리 미들웨어
+app.use((req, res, next) => {
+  const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+  error.status = 404;
+  next(error); // next 안에 error를 넣어주면 바로 에러 미들웨어 넘어감
+});
+
+// 에러 미들웨어
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+app.listen(app.get('port'), () => {
+  console.log(app.get('port'), '번 포트에서 대기중');
+});
