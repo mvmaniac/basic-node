e110bac09e4f71b196a7c2dda16ae94f0b942fce
yhs <mvmaniaz@gmail.com>
2021-01-12 23:43:41 +0900

    [추가] 웹 API 서버 만들기 완료

--- nodebird-api/src/routes/middlewares.js
+++ nodebird-api/src/routes/middlewares.js
@@ -1,3 +1,6 @@
+const jwt = require('jsonwebtoken');
+const RateLimit = require('express-rate-limit');
+
 // req, res, next 가 있으면 미들웨어...
 exports.isLoggedIn = (req, res, next) => {
   if (req.isAuthenticated()) {
@@ -14,4 +17,44 @@ exports.isNotLoggedIn = (req, res, next) => {
     const message = encodeURIComponent('로그인한 상태입니다.');
     res.redirect(`/?error=${message}`);
   }
+};
+
+exports.verifyToken = (req, res, next) => {
+  try {
+    req.decoded = jwt.verify(req.headers.authorization, process.env.JWT_SECRET);
+    return next();
+  } catch (error) {
+    if (error.name === 'TokenExpiredError') {
+      // 유효기간 초과
+      return res.status(419).json({
+        code: 419,
+        message: '토큰이 만료되었습니다'
+      });
+    }
+    // 사용자 정의 코드임
+    return res.status(401).json({
+      code: 401,
+      message: '유효하지 않은 토큰입니다'
+    });
+  }
+};
+
+exports.apiLimiter = new RateLimit({
+  // 현재 설정이 1분에 10번만 요청 가능
+  windowMs: 60 * 1000, // 1분
+  max: 10,
+  delayMs: 0,
+  handler(req, res) {
+    res.status(this.statusCode).json({
+      code: this.statusCode, // 기본값 429
+      message: '1분에 한 번만 요청할 수 있습니다.'
+    });
+  }
+});
+
+exports.deprecated = (req, res) => {
+  res.status(410).json({
+    code: 410,
+    message: '새로운 버전이 나왔습니다. 새로운 버전을 사용하세요.'
+  });
 };
