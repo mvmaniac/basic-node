bf5b0cf5a2a6bf09c6928682f9c3a8b7c2f1d064
yhs <mvmaniaz@gmail.com>
2021-03-07 15:24:42 +0900

    Add(nodebird): config.js, logger.js

---
+++ nodebird/src/config/config.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    username: 'dev',
+    password: 'dev',
+    database: 'nodejs',
+    host: '127.0.0.1',
+    dialect: 'mysql'
+  },
+  test: {
+    username: 'dev',
+    password: 'dev',
+    database: 'nodejs_test',
+    host: '127.0.0.1',
+    dialect: 'mysql'
+  },
+  production: {
+    username: 'dev',
+    password: 'dev',
+    database: 'nodejs',
+    host: '127.0.0.1',
+    dialect: 'mysql'
+  }
+};
