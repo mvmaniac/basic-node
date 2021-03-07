b5cf3dcaf3062b3e2c65404202be7aad9108ea10
yhs <mvmaniaz@gmail.com>
2021-03-07 15:25:41 +0900

    Update(example): path example

--- example/src/path.js
+++ example/src/path.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
-const pathJoin1 = path.join(__dirname, '/path.js'); // 절대경로 무시
-const pathJoin2 = path.join(__dirname, '..', 'path.js');
+const pathJoin1 = path.join(__dirname, '/path.JS'); // 절대경로 무시
+const pathJoin2 = path.join(__dirname, '../../', 'path.js');
 
 const pathResolve1 = path.resolve(__dirname, '/path.js'); // 절대경로가 있으면 앞의 경로 무시
 const pathResolve2 = path.resolve(__dirname, '..', 'path.js');
@@ -17,3 +17,11 @@ console.log(`pathResolve2: ${pathResolve2}`);
 console.log('========');
 
 console.log(`filename: ${__filename}`);
+
+console.log('========');
+
+console.log(`path.extname: ${path.extname(pathJoin1)}`);
+console.log(`path.dirname: ${path.dirname(pathJoin1)}`);
+console.log(
+  `path.filename: ${path.basename(pathJoin1, path.extname(pathJoin1))}`
+);
